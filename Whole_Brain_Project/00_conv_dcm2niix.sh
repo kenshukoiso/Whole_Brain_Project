4c24dbc697d829f72283e19e45fd12f148b5e1e2
Kenshu Koiso <90631737+kenshukoiso@users.noreply.github.com>
2022-02-28 15:17:17 +0100

    Delete Whole_Brain_Project directory

--- Whole_Brain_Project/00_conv_dcm2niix.sh
+++
@@ -1,4 +0,0 @@
-
-mkdir ../nii 
-#dcm2niix -b y -z y -i n  -o ../ ./
-dcm2niix -b y -z n -i n  -o ../nii ./
--- Whole_Brain_Project/s03_freesurfer_2nd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-pfad=$(pwd)
-SUBJECTS_DIR=$pfad
-recon-all -autorecon2-wm -autorecon3 -subjid S8 &
